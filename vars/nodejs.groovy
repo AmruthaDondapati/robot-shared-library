c1fc0592969d10e196c71e456077ef4336692c43
Amrutha <amruthadondapati3@gmail.com>
2023-12-25 13:10:55 +0530

    updated

---
+++ vars/nodejs.groovy
@@ -0,0 +1,24 @@
+// This is how we declare a function in groovy.
+def LintChecks(COMPONENT)) {
+    sh '''
+    echo "Installing JSlint"
+    echo "lint checks completed for $COMPONENT" '''
+}
+
+// Calling the info function and supplying both the values. 
+// LintChecks("DevOps", "DevOpsTraining.com")
+
+def call{COMPONENT} {
+    pipeline {
+        agent any 
+        stages {
+            stage ('LintChecks') {
+                steps {
+                    script {
+                        LintChecks(COMPONENT)
+                    }
+                }
+            }
+        }
+    }
+}
--- vars/sample.groovy
+++
@@ -1,7 +0,0 @@
-// This is how we declare a function in groovy.
-def LintChecks(message, URL) {
-    echo "INFO: ${message} and the URL is ${URL}"
-}
-
-// Calling the info function and supplying both the values. 
-LintChecks("DevOps", "DevOpsTraining.com")
