3b5d6f962dccb4d62be578a07040c9245e5ca5f2
Amrutha <amruthadondapati3@gmail.com>
2023-12-25 23:39:11 +0530

    sonarscanner updated

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -4,6 +4,10 @@ def LintChecks(COMPONENT) {
     sh "echo lint checks completed for ${COMPONENT}" 
 }
 
+def sonarChecks(COMPONENT) {
+    sh "echo checking sonarchecks"
+    sh "sonar-scanner -Dsonar.host.url=http://localhost:9000 -Dsonar.projectKey=myproject -Dsonar.sources=src1 -Dsonar.login=admin -Dsonar.password=zxc" 
+}
 // Calling the info function and supplying both the values. 
 // LintChecks("DevOps", "DevOpsTraining.com")
 
@@ -15,6 +19,13 @@ def call(COMPONENT) {
                 steps {
                     script {
                         LintChecks(COMPONENT)
+                    }
+                }
+            }
+            tage ('Sonarchecks') {
+                steps {
+                    script {
+                        SonarChecks(COMPONENT)
                     }
                 }
             }
