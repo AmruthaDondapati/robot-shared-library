a376b01f3418f0126e1cd8912d1b69b1ffe387c0
Amrutha <amruthadondapati3@gmail.com>
2023-12-27 11:05:22 +0530

    updated

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -6,7 +6,7 @@ def LintChecks(COMPONENT) {
 
 def SonarChecks(COMPONENT) {
     sh "echo Checking the Quality Checks"
-    sh "sonar-scanner -Dsonar.host.url=http://172.31.28.17:9000 -Dsonar.projectKey=$("COMPONENT") -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
+    sh "sonar-scanner -Dsonar.host.url=http://172.31.28.17:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
 }
 // Calling the info function and supplying both the values. 
 // LintChecks("DevOps", "DevOpsTraining.com")
