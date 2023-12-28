1781a97efd03cb05fdc3d9e83f4b60314617b219
Amrutha <amruthadondapati3@gmail.com>
2023-12-28 14:42:15 +0530

    updated

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -7,8 +7,8 @@ def LintChecks(COMPONENT) {
 def SonarChecks(COMPONENT) {
     sh "echo Checking the Quality Checks"
     sh "sonar-scanner -Dsonar.host.url=http://${sonar_URL}:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
-    sh  "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gata.sh"
-    sh  "bash -x quality-gata.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
+    sh  "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
+    sh  "bash -x quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
     sh " echo sonar checks completed for ${COMPONENT}.....!!!!!"
 }
 
